7ab84039f3d6a39bbce79a512d72dd90f6dd6264
Creepy <128508443+creepygonemad@users.noreply.github.com>
2025-07-29 02:33:34 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 import time
 import csv
